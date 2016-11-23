abfb519c17af275a0e51802f0ebaef82a07e5e11
Nagaraju <nagaraju.kukudala@gmail.com>
2016-11-23 12:14:26 +0530

    Add files via upload

---
+++ app7.1.js
@@ -0,0 +1,13 @@
+//Area of Circle with radius 10
+
+
+CalculateArea(10);
+
+function CalculateArea(radius){
+    
+  var area= 3.14 * radius *radius; // Formulae for area of Circle 
+  return( console.log( "Area of circle is "+ area));
+    
+}
+
+//console.log("Area of Circle with radius 5 is:" + area);
